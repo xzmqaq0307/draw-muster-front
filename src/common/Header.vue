6997487cb66a95274de51a040358fa1544f0ef24
Gogs <gogs@fake.local>
2020-02-02 22:15:01 +0800

    Initial commit

---
+++ src/common/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header class="w">
+      <div class="w-container">
+        <div class="nav-logo">
+          <h1 @click="changePage(-1)">
+            <router-link to="/" title="随画">随画——绘画交流区</router-link>
+          </h1>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    data() {
+      return {
+        activeIndex: '1',
+        choosePage: -1,
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      changePage (v) {
+        this.choosePage = v;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <d-header></d-header>
+    <router-view></router-view>
+    <d-footer></d-footer>
+  </div>
+</template>
+
+<script>
+
+    import DHeader from "../common/Header";
+    import DFooter from "../common/Footer";
+    export default {
+        name: "index",
+      components: {DHeader,DFooter}
+    }
+</script>
+
+<style scoped>
+
+</style>
